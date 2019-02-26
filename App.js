329b6356e3c59c93a3919ef3596541b8bbf3b5c5
Magnus Thor <magnus.thor.ar@gmail.com>
2019-02-26 08:15:56 +0100

    setup new react native project

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: {}
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
