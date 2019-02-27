a243fb7a0c6da282b5e430f4f5052e61a49606ae
Magnus Thor <magnus.thor.ar@gmail.com>
2019-02-27 12:55:48 +0100

    add service to fetch articles sets up flatlist to list articles

--- App.js
+++ App.js
@@ -1,18 +1,63 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { List, ListItem, Image } from "react-native-elements";
+import { getArticles } from "./services/fetchArticles";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      articles: {}
+      articles: [],
+      articlesFetched: false
     };
+  }
+
+  updateStateHandler(articles) {
+    this.setState({
+      articles: articles,
+      articlesFetched: true
+    });
+  }
+
+  componentDidMount() {
+    getArticles().then(res => {
+      this.updateStateHandler(res);
+    });
+  }
+
+  renderArticles(item) {
+    const article = item.item;
+    // if (this.state.articlesFetched) {
+    return (
+      <View>
+        <Image
+          style={{ width: 100, height: 100 }}
+          source={{ uri: article.image }}
+        />
+        <Text>{article.category.name}</Text>
+        <Text>{article.title}</Text>
+        <Text>{article.description}</Text>
+      </View>
+    );
+    // } else {
+    //   return (
+    //     <View>
+    //       <Text>NO Articles</Text>
+    //     </View>
+    //   );
+    // }
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!!!</Text>
+        <Text>Hello</Text>
+        <List>
+          <FlatList
+            data={this.state.articles}
+            renderItem={this.renderArticles.bind(this)}
+          />
+        </List>
       </View>
     );
   }
---
+++ services/fetchArticles.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const getArticles = async () => {
+  // const url = `http://192.168.1.178:3000/api/articles`;
+  const url = `https://ca-sunrise-rn.herokuapp.com/api/articles`;
+  try {
+    let response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    });
+    const articles = JSON.parse(response._bodyInit)["articles"];
+    return articles;
+  } catch (error) {
+    console.error(error);
+  }
+};
