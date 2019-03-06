144a3108e03c003fe5bb5354a4db0bac17bae46e
Magnus Thor <magnus.thor.ar@gmail.com>
2019-03-06 13:48:02 +0100

    WIP implementing login functionality

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Button } from "react-native";
 import { List, ListItem, Image } from "react-native-elements";
 import { getArticles } from "./services/fetchArticles";
+import LoginForm from "./Components/LoginForm";
+import { authenticate } from "./services/Auth";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       articles: [],
-      articlesFetched: false
+      articlesFetched: false,
+      renderLoginForm: false,
+      email: "",
+      password: "",
+      message: ""
     };
   }
 
@@ -25,9 +31,14 @@ export default class App extends React.Component {
     });
   }
 
+  onChange(event) {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
   renderArticles(item) {
     const article = item.item;
-    // if (this.state.articlesFetched) {
     return (
       <View>
         <Image
@@ -39,27 +50,60 @@ export default class App extends React.Component {
         <Text>{article.description}</Text>
       </View>
     );
-    // } else {
-    //   return (
-    //     <View>
-    //       <Text>NO Articles</Text>
-    //     </View>
-    //   );
-    // }
   }
 
+  async onLogin(e) {
+    console.log("CLICKED");
+    let resp = await authenticate(this.state.email, this.state.password);
+    if (resp.authenticated === true) {
+      this.setState({ authenticated: true });
+    } else {
+      this.setState({ message: resp.message, renderLoginForm: false });
+    }
+  }
+
+  renderLogin() {
+    let user;
+
+    if (this.state.authenticated === true) {
+      user = JSON.parse(sessionStorage.getItem("credentials")).uid;
+      return <p>Hi {user}</p>;
+    } else {
+      if (this.state.renderLoginForm === true) {
+        return (
+          <View>
+            <LoginForm
+              loginHandler={this.onLogin.bind(this)}
+              handleEmail={this.handleEmail}
+              handlePassword={this.handlePassword}
+              // inputChangeHandler={this.onChange.bind(this)}
+            />
+          </View>
+        );
+      } else {
+        return (
+          <>
+            <Button
+              title="Login"
+              onPress={() => this.setState({ renderLoginForm: true })}
+            />
+          </>
+        );
+      }
+    }
+  }
+
+  handleEmail = text => {
+    this.setState({ email: text });
+  };
+  handlePassword = text => {
+    this.setState({ password: text });
+  };
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-        <List>
-          <FlatList
-            data={this.state.articles}
-            renderItem={this.renderArticles.bind(this)}
-          />
-        </List>
-      </View>
-    );
+    let renderLogin = this.renderLogin();
+
+    return <View style={styles.container}>{renderLogin}</View>;
   }
 }
 
--- services/fetchArticles.js
+++ services/fetchArticles.js
@@ -4,13 +4,7 @@ export const getArticles = async () => {
   // const url = `http://192.168.1.178:3000/api/articles`;
   const url = `https://ca-sunrise-rn.herokuapp.com/api/articles`;
   try {
-    let response = await fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
-    });
+    let response = await fetch(url);
     const articles = JSON.parse(response._bodyInit)["articles"];
     return articles;
   } catch (error) {
